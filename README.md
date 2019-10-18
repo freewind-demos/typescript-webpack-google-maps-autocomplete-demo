b7dbb1cd599028dbb5f5cb611094f953270dcf31
Peng Li <peng.li@compass.com>
2019-10-18 16:03:00 +0800

    add clear

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ TypeScript Webpack Google Maps AutoComplete Demo
 
 注意`index.html`中引用的google js里，需要加上`libraries=places`
 
+还增加了一个clear功能，这个功能AutoComplete官方没有提供，使用了一些变通的方式实现的。
+
 需要自己申请一个key
 
 ```
--- src/entry.ts
+++ src/entry.ts
@@ -1,10 +1,36 @@
-function main() {
-  const input = document.getElementById('autocomplete')!;
+const input = document.getElementById('autocomplete')! as HTMLInputElement;
+
+function setupAutoComplete(): google.maps.places.Autocomplete {
+
   const autocomplete = new google.maps.places.Autocomplete(input as HTMLInputElement);
   google.maps.event.addListener(autocomplete, 'place_changed', function () {
     const myLocation = autocomplete.getPlace();
     console.log("myLocation", myLocation);
+  });
+
+  input.addEventListener('blur', (event: Event) => {
+    const value = (event.target as HTMLInputElement).value;
+    if (value.trim() === '') {
+      autocomplete.set('place', null);
+    }
+  });
+
+  return autocomplete;
+}
+
+function setupClear(autoComplete: google.maps.places.Autocomplete): void {
+  const button = document.getElementById('clear')!;
+  button.addEventListener('click', () => {
+    input.value = '';
+    input.focus();
+    input.blur();
   })
 }
 
+function main() {
+  const autoComplete = setupAutoComplete();
+  setupClear(autoComplete);
+}
+
 main();
+
